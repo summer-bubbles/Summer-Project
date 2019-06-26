4c86d446aab74d2b143375d6fcd5a47c09f2d2ad
Danil Khrustov <khrustovd@gmail.com>
2019-06-26 14:20:09 -0500

    refactored the sprite collision code

--- index.js
+++ index.js
@@ -121,6 +121,7 @@ function setup() {
   //Start the game loop
   app.ticker.add(delta => gameLoop(delta));
   id = PIXI.loader.resources['/tankSheet.json'].textures;
+
   for (let i = 0; i <= 4; i++) {
     for (let j = 0; j <= 4; j++) {
       let grass = new Sprite(id['grass03.png']);
@@ -275,104 +276,38 @@ function shoot(rotation, startPosition) {
   bullets.push(bullet);
 }
 
-function rotateToPoint(mx, my, px, py) {
-  var dist_Y = my - py;
-  var dist_X = mx - px;
-  var angle = Math.atan2(dist_Y, dist_X);
-  return angle;
-}
-
-function getVisualMetrics() {
-  return {
-    height: app.renderer.height,
-    width: app.renderer.width,
-    xmin: 0,
-    xmax: app.renderer.width - 1,
-    ymin: 0,
-    ymax: app.renderer.height - 1,
-  };
-}
-
 function hitTestRectangle(r1, r2) {
-  //Define the variables we'll need to calculate
   let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
 
-  //hit will determine whether there's a collision
   hit = false;
 
-  //Find the center points of each sprite
   r1.centerX = r1.x + r1.width / 2;
   r1.centerY = r1.y + r1.height / 2;
   r2.centerX = r2.x + r2.width / 2;
   r2.centerY = r2.y + r2.height / 2;
 
-  //Find the half-widths and half-heights of each sprite
   r1.halfWidth = r1.width / 2;
   r1.halfHeight = r1.height / 2;
   r2.halfWidth = r2.width / 2;
   r2.halfHeight = r2.height / 2;
 
-  //Calculate the distance vector between the sprites
   vx = r1.centerX - r2.centerX;
   vy = r1.centerY - r2.centerY;
 
-  //Figure out the combined half-widths and half-heights
   combinedHalfWidths = r1.halfWidth + r2.halfWidth;
   combinedHalfHeights = r1.halfHeight + r2.halfHeight;
 
-  //Check for a collision on the x axis
   if (Math.abs(vx) < combinedHalfWidths) {
-    //A collision might be occurring. Check for a collision on the y axis
+    
     if (Math.abs(vy) < combinedHalfHeights) {
-      //There's definitely a collision happening
       hit = true;
     } else {
-      //There's no collision on the y axis
       hit = false;
     }
   } else {
-    //There's no collision on the x axis
     hit = false;
   }
-
-  //`hit` will be either `true` or `false`
   return hit;
-}
-
-//spriteCollision helper func that checks for sprite boundaries
-function spriteCollision(sprite, object) {
-  let collision = undefined;
-
-  //Left
-  if (sprite.x < object.x) {
-    sprite.vx = 0;
-    //sprite.x = object.x;
-    collision = 'left';
-  }
-
-  //Top
-  if (sprite.y < object.y) {
-    sprite.vy = 0;
-    //sprite.y = container.y;
-    collision = 'top';
-  }
-
-  //Right
-  if (sprite.x + sprite.width > object.width) {
-    sprite.vx = 0;
-    //sprite.x = container.width - sprite.width;
-    collision = 'right';
-  }
-
-  //Bottom
-  if (sprite.y + sprite.height > object.height) {
-    sprite.vy = 0;
-    //sprite.y = container.height - sprite.height;
-    collision = 'bottom';
-  }
-
-  //Return the `collision` value
-  return collision;
 }
 
 //Contain Helper Function For Boundries around sprite!
